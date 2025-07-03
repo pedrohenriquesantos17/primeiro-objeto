bebd76b48f8b9d5c9c74fef7bb9d30b27e1e7162
pedrohenriquesantos17 <pedro.silva1704@escola.pr.gov.br>
2025-07-03 08:38:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function chama_aluno(){
+    let aluno = {
+    nome: 'pedro henrique',
+        idade: 15,
+        altura: 1.80,
+        nota: 80.0
+    };
+    //alert(aluno.nome);
+     document.getElementById('nome_aluno').textContent = 'Nome: ' + aluno.nome;
+       document.getElementById('nome_aluno').textContent = 'Nome: ' + aluno.idade;
+       document.getElementById('nome_aluno').textContent = 'Nome: ' + aluno.altura;
+        document.getElementById('nome_aluno').textContent = 'Nome: ' + aluno.nota;
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
